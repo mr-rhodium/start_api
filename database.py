6bf87051cddc8964dbf6b6db52dce7efb0405dcc
Taras Nytiaha <taras.nytiaha@gmail.com>
2024-10-05 17:59:26 +0200

    fix: alembic

--- database.py
+++ database.py
@@ -2,7 +2,12 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine(os.environ.get("DATABASE_URL"))
+pg_user = os.getenv("DATABASE_URL")
+pg_password = os.getenv("DATABASE_PASSWORD")
+pg_database = os.getenv("DATABASE_NAME")
+
+database_url = f"postgresql://{pg_user}:{pg_password}@db:5432/{pg_database}"
+engine = create_engine(database_url)
 
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
